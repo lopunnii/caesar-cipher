76515f9d26c993e4314411e1c639c4031991069e
Lopunnii <kimi7@xtra.co.nz>
2022-12-16 16:47:41 +0900

    Add functionality for strings with spaces and symbols

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,24 +1,3 @@
-=begin
-def caesar_cipher(string, key)
-
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    
-    if key > 25 || key < -25
-        return puts "Please input a key that is between -25 and 25"
-    end
-
-    shifted_index = alphabet.index(string) + key
-
-    if shifted_index > 25
-        shifted_index -= 26
-    end
-
-    p shifted_index
-    p alphabet[shifted_index]
-end
-=end
-
 def caesar_cipher(string, key)
 
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -29,16 +8,36 @@ def caesar_cipher(string, key)
     end
 
     arr = string.split(//)
-    arr2 = arr.map {|letter| letter = alphabet.index(letter) + key}
-    arr3 = arr2.map { |index|
-        if index > 25
-            index -= 26
+    p arr
+    
+    arr2 = arr.map {|letter| 
+        if letter =~ /[a-zA-Z]/
+        letter = alphabet.index(letter) + key
+        else
+        letter = letter
         end
-        index = alphabet[index]
     }
-    arr3.join
+    p arr2
+
+    arr3 = arr2.map { |index|
+        index = index.to_s   
+    }
+    p arr3
+
+    arr4 = arr3.map { |index|
+        if index =~ /[0-9]/
+            index = index.to_i
+            if index > 25
+                index -= 26
+            end
+            index = alphabet[index]
+        else
+            index = index
+        end
+    }
+
+    p arr4
+    arr4.join
 end
 
-p caesar_cipher("l o v e", 5)
-
-p "i love you".split(//)
+p caesar_cipher("acc dee!", 5)
